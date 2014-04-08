ff8681ccfa72ea243dc586912a53e23ac0e2c060
Puneet <puneet.behl@intelligrape.com>
2014-04-08 21:42:55 +0530

    Defined resource modeules.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,27 @@
 modules = {
+    core {
+        dependsOn 'bootstrap, holder, font_awesome, application'
+        defaultBundle 'ui'
+        resource url: '/css/docs.min.css'
+    }
+
     application {
-        resource url:'js/application.js'
+        dependsOn 'jquery'
+        resource url: 'js/application.js'
+    }
+
+    bootstrap {
+        dependsOn 'jquery'
+        resource url: '/css/bootstrap.min.css'
+        resource url: '/js/bootstrap.min.js'
+    }
+
+    holder {
+        dependsOn 'jquery'
+        resource url: '/js/holder.js'
+    }
+
+    font_awesome {
+        resource url: 'css/font-awesome.min.css'
     }
 }
