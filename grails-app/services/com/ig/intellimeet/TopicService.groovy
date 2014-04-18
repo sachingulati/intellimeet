fecced3165483b12c268bd44274a30b4d4dd955b
Puneet <puneet.behl@intelligrape.com>
2014-04-18 08:28:04 +0530

    Implemented search functionality on topic board.

--- grails-app/services/com/ig/intellimeet/TopicService.groovy
+++ grails-app/services/com/ig/intellimeet/TopicService.groovy
@@ -1,5 +1,9 @@
 package com.ig.intellimeet
 
+import com.ig.intellimeet.dto.CategoryTopicCount
+import com.mongodb.AggregationOutput
+import com.mongodb.DBCollection
+
 class TopicService {
     def springSecurityService
 
@@ -7,5 +11,11 @@ class TopicService {
         User user = springSecurityService.currentUser as User
         topic.interestedUsers << user?.id
         topic.interestedUsers?.size()
+    }
+
+    List<CategoryTopicCount> countTopicGroupedByCategory() {
+        DBCollection topicCollection = Topic.collection
+        AggregationOutput aggregationOutput = topicCollection?.aggregate(['$group': ['_id': '$category', 'count': ['$sum': 1]]])
+        aggregationOutput?.results()?.collect {new CategoryTopicCount(category: it['_id'], topicCount: it['count'])}
     }
 }
