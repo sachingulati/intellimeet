5f60f63de272849eb03df7a425649d49a6a8f727
Mohd Farid <farid@intelligrape.com>
2014-05-01 12:16:09 +0530

    Added created by id in the filters to all save actions.

--- grails-app/conf/applicationFilter/AppFilterFilters.groovy
+++ grails-app/conf/applicationFilter/AppFilterFilters.groovy
@@ -2,7 +2,22 @@ package applicationFilter
 
 class AppFilterFilters {
 
+    def springSecurityService
     def filters = {
+
+        saveActions(controller:'*', action:'save') {
+            before = {
+                params.createdBy = springSecurityService.currentUser?.id
+                println("AppFilter : Params: ${params}")
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+
         all(controller:'*', action:'*') {
             before = {
                 println("AppFilter : Params: ${params}")
