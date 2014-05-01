7d0681981d373ca9911006aafa46831939d8099a
Puneet <puneet.behl@intelligrape.com>
2014-05-01 11:59:23 +0530

    Implemented class named SessionPreference.groovy specl. for displaying all the preferences grouped by 1st,2nd & 3rd for specific session.

---
+++ src/groovy/com/ig/intellimeet/dto/SessionPreference.groovy
@@ -0,0 +1,10 @@
+package com.ig.intellimeet.dto
+
+class SessionPreference {
+
+    Long sessionId
+    List<Long> preferenceOneUserIds
+    List<Long> preferenceTwoUserIds
+    List<Long> preferenceThreeUserIds
+
+}
