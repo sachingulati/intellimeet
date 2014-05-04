7e481f51ba0666f9c3356a9e0dac150153c96ff7
Puneet <puneet.behl@intelligrape.com>
2014-05-04 22:55:58 +0530

    Created UserPreferenceCO.groovy for saving user preferences for sessions.

---
+++ src/groovy/com/ig/intellimeet/co/UserPreferenceCO.groovy
@@ -0,0 +1,13 @@
+package com.ig.intellimeet.co
+
+import grails.validation.Validateable
+
+@Validateable
+class UserPreferenceCO {
+
+    String tokenId
+    Long firstPreferredSessionId
+    Long secondPreferredSessionId
+    Long thirdPreferredSessionId
+
+}
