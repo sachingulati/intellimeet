7dc4289bd9ca7ff68a517a754cb2be06e2520285
Puneet <puneet.behl@intelligrape.com>
2014-05-06 10:01:45 +0530

    Added userId field in SurveyRecipientInfo.

--- src/groovy/com/ig/intellimeet/embedded/SurveyRecipientInfo.groovy
+++ src/groovy/com/ig/intellimeet/embedded/SurveyRecipientInfo.groovy
@@ -1,16 +1,23 @@
 package com.ig.intellimeet.embedded
 
+import com.ig.intellimeet.User
 import com.ig.intellimeet.enums.SurveyStatus
 import grails.validation.Validateable
 
 @Validateable
 class SurveyRecipientInfo {
 
+    Long userId
     String email
     SurveyStatus status
 
     static constraints = {
         email email: true, blank: false
         status nullable: true
+    }
+
+    void setUserId(Long userId) {
+        this.userId = userId
+        this.email = User.get(userId)?.username
     }
 }
