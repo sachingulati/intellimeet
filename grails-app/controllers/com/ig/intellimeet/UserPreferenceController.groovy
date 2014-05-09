636d1221b0079ad0050c9e66b2e44144a4e1e599
Puneet <puneet.behl@intelligrape.com>
2014-05-10 00:57:19 +0530

    Removed show action from UserPreferenceController.groovy

--- grails-app/controllers/com/ig/intellimeet/UserPreferenceController.groovy
+++ grails-app/controllers/com/ig/intellimeet/UserPreferenceController.groovy
@@ -1,6 +1,5 @@
 package com.ig.intellimeet
 import com.ig.intellimeet.co.UserPreferenceCO
-import com.ig.intellimeet.enums.SessionStatus
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
 
@@ -10,31 +9,16 @@ class UserPreferenceController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
     def userPreferenceService
-    def tokenService
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond UserPreference.list(params), model: [userPreferenceInstanceCount: UserPreference.count()]
     }
 
-    def show(String tokenId) {
-        Token token = tokenService.extractToken(tokenId)
-        if(!token) {
-            respond null
-        }
-        [sessions: IMSession.findAllByIntelliMeetIdAndSessionStatus(token?.intelliMeetId, SessionStatus.LIVE), tokenId: tokenId]
-    }
-
     @Transactional
     def save(UserPreferenceCO userPreferenceCO) {
-        Token token = tokenService.extractToken(userPreferenceCO?.tokenId)
-        if(!token) {
-            respond null
-        }
-        else if(!userPreferenceCO?.hasErrors()) {
+        if(!userPreferenceCO?.hasErrors()) {
             UserPreference userPreference  = userPreferenceService.extractUserPreference(userPreferenceCO)
-            userPreference.userId = token?.userId
-            userPreference.intelliMeetId = token?.intelliMeetId
             userPreferenceService.save userPreference
         }
     }
