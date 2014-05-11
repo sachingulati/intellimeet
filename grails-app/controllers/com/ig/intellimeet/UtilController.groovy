9edbcb669f2478300dd76677c0182ce7b062d0e9
Puneet <puneet.behl@intelligrape.com>
2014-05-11 17:00:44 +0530

    Secured UtilController.groovy for some specific users.

--- grails-app/controllers/com/ig/intellimeet/UtilController.groovy
+++ grails-app/controllers/com/ig/intellimeet/UtilController.groovy
@@ -1,6 +1,18 @@
 package com.ig.intellimeet
 
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_USER'])
 class UtilController {
+
+    def surveyService
+    def springSecurityService
+
+    def beforeInterceptor = {
+        if(!['farid@intelligrape.com', 'puneet.behl@intelligrape.com', 'mohit@intelligrape.com']?.contains(springSecurityService?.currentUser?.username)) {
+            return
+        }
+    }
 
     def checkHealth() {
         render("Success")
@@ -9,5 +21,10 @@ class UtilController {
     def getUserList() {
         List<Long> userIds = Topic.list()*.interestedUsers?.unique()?.flatten()
         render(User.getAll(userIds)*.username)
+    }
+
+    def sendTestEmail( ) {
+        grailsApplication.config.grails.mail.disabled=false
+        surveyService.sendSurveyEmail(params.email)
     }
 }
