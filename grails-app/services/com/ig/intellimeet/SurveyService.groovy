2fa1a0c4f47a8b367e92a911ed345a84458495e0
Puneet <puneet.behl@intelligrape.com>
2014-05-11 15:46:29 +0530

    Implemented sendEmail service method in SurveyService.groovy

--- grails-app/services/com/ig/intellimeet/SurveyService.groovy
+++ grails-app/services/com/ig/intellimeet/SurveyService.groovy
@@ -1,18 +1,38 @@
 package com.ig.intellimeet
 
+import com.ig.intellimeet.dto.MailDTO
 import com.ig.intellimeet.embedded.SurveyRecipientInfo
+import com.ig.intellimeet.enums.SurveyStatus
 
 class SurveyService {
 
-    def tokenService
     def imMailService
+    def intelliMeetService
 
     def sendSurveyEmail(Survey survey) {
         survey?.recipients?.each {SurveyRecipientInfo surveyRecipientInfo->
-            Token token = tokenService.generateToken(surveyRecipientInfo?.userId)
-            if(token && tokenService.save(token)) {
-            }
+            sendSurveyEmail(surveyRecipientInfo?.email)
+            surveyRecipientInfo?.status = SurveyStatus.SENT
         }
     }
 
+    def sendSurveyEmail(String emailAddress) {
+        if(emailAddress) {
+            MailDTO mailDTO = new MailDTO()
+            mailDTO.to = emailAddress
+            mailDTO.subject = subjectForPreferenceEmail
+            mailDTO.body = messageForPreferenceEmail
+            log.info("Sending preference email to ${mailDTO?.to}")
+            imMailService.sendMail(mailDTO)
+        }
+    }
+
+
+    String getSubjectForPreferenceEmail() {
+        Survey.SAMPLE_SURVEY_SUBJECT_FOR_SESSION_PREFERENCE
+    }
+
+    String getMessageForPreferenceEmail() {
+        Survey.SAMPLE_SURVEY_MESSAGE_FOR_SESSION_PREFERENCE
+    }
 }
