cbdcbf1f0b698f83b2ddd85c1dea986916d908bf
Puneet <puneet.behl@intelligrape.com>
2014-05-13 07:38:36 +0530

    Created action in UtilController to view pref.

--- grails-app/controllers/com/ig/intellimeet/UtilController.groovy
+++ grails-app/controllers/com/ig/intellimeet/UtilController.groovy
@@ -1,14 +1,16 @@
 package com.ig.intellimeet
 
+import com.ig.intellimeet.enums.SessionStatus
 import com.ig.intellimeet.utils.TestUtil
 
 class UtilController {
 
     def surveyService
     def springSecurityService
+    def intelliMeetService
 
     def beforeInterceptor = {
-        if(!['farid@intelligrape.com', 'puneet.behl@intelligrape.com']?.contains(springSecurityService?.currentUser?.username)) {
+        if (!['farid@intelligrape.com', 'puneet.behl@intelligrape.com']?.contains(springSecurityService?.currentUser?.username)) {
             return
         }
     }
@@ -17,8 +19,8 @@ class UtilController {
         render("Success")
     }
 
-    def sendTestEmail( ) {
-        grailsApplication.config.grails.mail.disabled=false
+    def sendTestEmail() {
+        grailsApplication.config.grails.mail.disabled = false
         surveyService.sendSurveyEmail(params.email)
     }
 
@@ -37,5 +39,10 @@ class UtilController {
     def thankyou() {
         render view: '/survey/thankyou'
         return
+    }
+
+    def pref() {
+        Boolean hasFilledPreferences = false
+        render view: '/survey/session', model: [sessions: IMSession.findAllByIntelliMeetIdAndSessionStatus(intelliMeetService?.currentIntelliMeetId, SessionStatus.PROPOSED), hasFilledPreferences: hasFilledPreferences, tokenId: "Dummy Token"]
     }
 }
