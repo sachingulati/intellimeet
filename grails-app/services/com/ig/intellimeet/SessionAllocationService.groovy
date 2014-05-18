94351c307c6b6916eb89652f1f4988307a481a48
Puneet <puneet.behl@intelligrape.com>
2014-05-18 22:02:55 +0530

    Implemented service method in SessionAllocationService.groovy named "generateAllocationCO".

--- grails-app/services/com/ig/intellimeet/SessionAllocationService.groovy
+++ grails-app/services/com/ig/intellimeet/SessionAllocationService.groovy
@@ -1,12 +1,15 @@
 package com.ig.intellimeet
 
+import com.ig.intellimeet.co.AllocationCO
+import com.ig.intellimeet.dto.PreferenceDTO
 import com.ig.intellimeet.dto.SessionPreference
+import com.ig.intellimeet.dto.SessionPreferenceDTO
 import grails.transaction.Transactional
 
 @Transactional
 class SessionAllocationService {
 
-    IntelliMeetService intelliMeetService
+    def intelliMeetService
 
     /**
      * <p>Returns the session preference of users for current intellimeet
@@ -25,34 +28,34 @@ class SessionAllocationService {
         userPreferences.each { UserPreference userPreference ->
 
             SessionPreference firstSessionPreference = sessionPreferenceMap.get(userPreference.firstPreferredSessionId)
-            if(firstSessionPreference == null) {
+            if (firstSessionPreference == null) {
                 firstSessionPreference = newSessionPreference(userPreference, currentIntelliMeetId)
                 firstSessionPreference.sessionId = userPreference.firstPreferredSessionId
-                firstSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.firstPreferredSessionId,currentIntelliMeetId)
+                firstSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.firstPreferredSessionId, currentIntelliMeetId)
                 sessionPreferenceMap.put(firstSessionPreference.sessionId, firstSessionPreference)
             }
             firstSessionPreference?.preferenceOneUserIds?.add(userPreference.userId)
 
             SessionPreference secondSessionPreference = sessionPreferenceMap.get(userPreference.secondPreferredSessionId)
-            if(secondSessionPreference == null) {
+            if (secondSessionPreference == null) {
                 secondSessionPreference = newSessionPreference(userPreference, currentIntelliMeetId)
                 secondSessionPreference.sessionId = userPreference.secondPreferredSessionId
-                secondSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.secondPreferredSessionId,currentIntelliMeetId)
+                secondSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.secondPreferredSessionId, currentIntelliMeetId)
                 sessionPreferenceMap.put(secondSessionPreference.sessionId, secondSessionPreference)
             }
             secondSessionPreference?.preferenceTwoUserIds?.add(userPreference.userId)
 
             SessionPreference thirdSessionPreference = sessionPreferenceMap.get(userPreference.thirdPreferredSessionId)
-            if(thirdSessionPreference == null) {
+            if (thirdSessionPreference == null) {
                 thirdSessionPreference = newSessionPreference(userPreference, currentIntelliMeetId)
                 thirdSessionPreference.sessionId = userPreference.thirdPreferredSessionId
-                thirdSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.thirdPreferredSessionId,currentIntelliMeetId)
+                thirdSessionPreference.sessionTitle = UserPreference.findSessionTitleByIdAndIntelliMeetId(userPreference.thirdPreferredSessionId, currentIntelliMeetId)
                 sessionPreferenceMap.put(thirdSessionPreference.sessionId, thirdSessionPreference)
             }
             thirdSessionPreference?.preferenceThreeUserIds?.add(userPreference.userId)
 
-            if(firstSessionPreference.sessionId.equals(userPreference.purposedSessionId)) {
-                 firstSessionPreference.purposedAttendees.add(userPreference.userId)
+            if (firstSessionPreference.sessionId.equals(userPreference.purposedSessionId)) {
+                firstSessionPreference.purposedAttendees.add(userPreference.userId)
             } else if (secondSessionPreference.sessionId.equals(userPreference.purposedSessionId)) {
                 secondSessionPreference.purposedAttendees.add(userPreference.userId)
             } else if (thirdSessionPreference.sessionId.equals(userPreference.purposedSessionId)) {
@@ -62,7 +65,45 @@ class SessionAllocationService {
         return sessionPreferenceMap
     }
 
-    UserPreference savePurposedSessionAllocation(Integer sessionId, List attendeeIds){
+    AllocationCO generateAllocationCO(List<UserPreference> userPreferenceList) {
+        AllocationCO allocationCO = new AllocationCO()
+        PreferenceDTO preferenceDTO
+        userPreferenceList?.each { UserPreference userPreference ->
+            preferenceDTO=new PreferenceDTO(value: userPreference?.userId)
+            SessionPreferenceDTO firstSessionPreferenceDTO = allocationCO?.preferenceDTOList?.find { it.sessionId == userPreference?.firstPreferredSessionId }
+            if (!firstSessionPreferenceDTO) {
+                firstSessionPreferenceDTO = new SessionPreferenceDTO(sessionId: userPreference?.firstPreferredSessionId, sessionTitle: userPreference?.firstPreferredSessionTitle, firstPreferenceUserIdList: [], secondPreferenceUserIdList: [], thirdPreferenceUserIdList: [])
+                firstSessionPreferenceDTO?.firstPreferenceUserIdList << preferenceDTO
+                allocationCO?.preferenceDTOList?.add firstSessionPreferenceDTO
+            } else {
+                firstSessionPreferenceDTO?.firstPreferenceUserIdList << preferenceDTO
+            }
+
+            SessionPreferenceDTO secondSessionPreferenceDTO = allocationCO?.preferenceDTOList?.find { it.sessionId == userPreference?.secondPreferredSessionId }
+            if (!secondSessionPreferenceDTO) {
+                secondSessionPreferenceDTO = new SessionPreferenceDTO(sessionId: userPreference?.secondPreferredSessionId, sessionTitle: userPreference?.secondPreferredSessionTitle, firstPreferenceUserIdList: [], secondPreferenceUserIdList: [], thirdPreferenceUserIdList: [])
+                secondSessionPreferenceDTO?.secondPreferenceUserIdList << preferenceDTO
+                allocationCO?.preferenceDTOList?.add secondSessionPreferenceDTO
+            } else {
+                secondSessionPreferenceDTO?.secondPreferenceUserIdList << preferenceDTO
+            }
+
+
+            SessionPreferenceDTO thirdSessionPreferenceDTO = allocationCO?.preferenceDTOList?.find { it.sessionId == userPreference?.thirdPreferredSessionId }
+            if (!thirdSessionPreferenceDTO) {
+                thirdSessionPreferenceDTO = new SessionPreferenceDTO(sessionId: userPreference?.thirdPreferredSessionId, sessionTitle: userPreference?.thirdPreferredSessionTitle, firstPreferenceUserIdList: [], secondPreferenceUserIdList: [], thirdPreferenceUserIdList: [])
+                thirdSessionPreferenceDTO?.thirdPreferenceUserIdList << preferenceDTO
+                allocationCO?.preferenceDTOList?.add thirdSessionPreferenceDTO
+            } else {
+                thirdSessionPreferenceDTO?.thirdPreferenceUserIdList << preferenceDTO
+            }
+
+        }
+        allocationCO
+    }
+
+
+    UserPreference savePurposedSessionAllocation(Integer sessionId, List attendeeIds) {
         Long intelliMeetId = intelliMeetService.getCurrentIntelliMeetId()
         UserPreference userPreference = null
         attendeeIds.each { id ->
@@ -74,7 +115,7 @@ class SessionAllocationService {
         userPreference
     }
 
-    IMSession freezeSessionAllocation(Integer sessionId, List attendeeIds){
+    IMSession freezeSessionAllocation(Integer sessionId, List attendeeIds) {
         IntelliMeet intelliMeet = intelliMeetService.getCurrentIntelliMeet()
         IMSession imSession = IMSession.findByIntelliMeetIdAndId(intelliMeet.id, sessionId)
         imSession.attendeeIds = []
@@ -85,7 +126,7 @@ class SessionAllocationService {
         imSession
     }
 
-    private SessionPreference newSessionPreference (UserPreference userPreference, Long currentIntelliMeetId){
+    private SessionPreference newSessionPreference(UserPreference userPreference, Long currentIntelliMeetId) {
         SessionPreference sessionPreference = new SessionPreference()
         sessionPreference.preferenceOneUserIds = []
         sessionPreference.preferenceTwoUserIds = []
