e37eff59de14d0edeb179abdac8aafabddc5f6e2
Jeevesh <jeevesh@intelligrape.com>
2014-07-18 01:15:26 +0530

    Implementation: Tag to show user image.

--- grails-app/taglib/com/ig/intellimeet/UserTagLib.groovy
+++ grails-app/taglib/com/ig/intellimeet/UserTagLib.groovy
@@ -3,8 +3,14 @@ package com.ig.intellimeet
 class UserTagLib {
 
     static namespace = "user"
+    def grailsApplication
 
     def displayUsername = { attrs ->
         out << User.get(attrs.userId)?.username
+    }
+
+    def displayUserImage = { attrs ->
+        String imageUrl = grailsApplication.config.cloudinary.baseUrl + attrs.email + grailsApplication.config.cloudinary.mimeType
+        out << "<img src='${imageUrl}' height='100px' width='100px'/>"
     }
 }
