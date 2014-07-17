14bb9025707da5ce522fc44de97416ad757357d1
Jeevesh <jeevesh@intelligrape.com>
2014-07-18 01:07:28 +0530

    Implementation: Functionality to save user image.

--- grails-app/controllers/com/ig/intellimeet/UserController.groovy
+++ grails-app/controllers/com/ig/intellimeet/UserController.groovy
@@ -1,13 +1,30 @@
 package com.ig.intellimeet
 
 import grails.plugin.springsecurity.annotation.Secured
-
-import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import org.springframework.web.multipart.commons.CommonsMultipartFile
 
 @Transactional(readOnly = true)
 @Secured(['ROLE_ADMIN'])
 class UserController {
 
-  static scaffold = true
+    def cloudinaryService
+    def springSecurityService
+    def grailsApplication
+    static scaffold = true
+
+    def uploadImage() {
+    }
+
+    def saveImage() {
+        CommonsMultipartFile userImage = params.userImage
+        log.info("Image name ${userImage.originalFilename}")
+        User currentUser = springSecurityService?.currentUser
+        try {
+            cloudinaryService.uploadImage(userImage.bytes, grailsApplication.config.cloudinary.dir as String, currentUser?.username, ['user', 'image'])
+        } catch (Exception e) {
+            log.info("Exception occurred while saving image")
+        }
+        redirect controller: 'user', action: 'uploadImage'
+    }
 }
